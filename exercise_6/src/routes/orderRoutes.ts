ea1af3c9600118966fbfbd4aa130122242ead5ce
Joyesh Malik <joyesh.malik@tothenew.com>
2024-05-22 11:12:55 +0530

    generating pdf using pupeteer

--- exercise_6/src/routes/orderRoutes.ts
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const orderController = require("../controllers/orderController")
-const authMiddleware = require("../middleware/authMiddleware")
-
-router.post("/place", authMiddleware, orderController.placeOrder)
-router.get("/", authMiddleware, orderController.getOrder)
-
-module.exports = router
--- exercise_6/src/routes/promotionalEmailRoutes.ts
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const {
-  sendPromotionalEmailController,
-} = require("../controllers/promotionalEmailController")
-
-router.post("/", sendPromotionalEmailController)
-
-module.exports = router
