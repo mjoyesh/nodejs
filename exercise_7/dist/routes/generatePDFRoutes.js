93d855884a7b2e2794ab3568c7791084e56d186c
Joyesh Malik <joyesh.malik@tothenew.com>
2024-05-22 17:19:46 +0530

    removed dist file

--- exercise_7/dist/routes/generatePDFRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router = express.Router();
-const generatePPDFController = require("../controllers/generatePDF");
-const authMiddleware = require("../middleware/authMiddleware");
-router.get("/", authMiddleware, generatePPDFController.generatePDF);
-module.exports = router;
-//# sourceMappingURL=generatePDFRoutes.js.map
